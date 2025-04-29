6b9616a1fc2c015b8aa37b4f8294c39de1937677
MSOFT-INFO <rakotonirinabjr@gmail.com>
2025-04-29 17:37:37 +0300

    seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,23 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create(
+                [
+                    'firstname' => "Admin",
+                    'lastname' => "Management",
+                    'email' => "admin@admin.com",
+                    'phone' => "0345811858",
+                    'dateofbirth' => date('Y-m-d',strtotime('12-02-1991')),
+                    'birthplace_postcode' => "111",
+                    'birthplace_city' => "Antsirabe II",
+                    'address_street' => "rue Jean Ralaimongo",
+                    'address_postcode' => "110",
+                    'address_city' => "Antsirabe",
+                    'email_verified_at' => now(),
+                    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                
+                ]
+            );
+        $user->assignRole('admin');
     }
 }
